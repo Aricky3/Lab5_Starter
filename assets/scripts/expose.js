2d12bbe142de80f97d19c7613f9d70a2fd497d23
Ricky Aguilar <r3aguila@ucsd.edu>
2022-10-28 19:31:04 -0700

    Comit 1

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,17 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  if(document.getElementById("horn-select").value == "car-horn"){
+    document.getElementsByTagName("img")[0].src = "assets/images/car-horn.svg"; /*img stored in 2D arr */
+    document.getElementsByTagName("audio")[0].src = "assets/audio/car-horn.mp3";
+    var x = document.querySelector("#volume-controls input");
+    if(x.value == 0){
+      document.getElementsByTagName("img")[1].src = "assets/icons/volume-level-0.svg";
+    }
+    var b = document.getElementsByTagName("button");
+    b.addEventListener("click",play);
+    function play(){
+      document.getElementsByTagName("audio").src.play();
+    }
+  }
 }
